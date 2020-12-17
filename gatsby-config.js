63780626bc898aaab3c8a25039de71d61650b6c9
Justin Chu <jchu231@gmail.com>
2020-12-16 19:35:25 -0800

    adding plausible.io plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,6 +72,13 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
+    // Plausible.io integration
+    {
+      resolve: `gatsby-plugin-plausible`,
+      options: {
+        domain: `smalltechbusiness.com`,
+      },
+    },
   ],
 }
