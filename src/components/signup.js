831fb71b6ff91bd252703cfb5005cfdba74da52c
Justin Chu <jchu231@gmail.com>
2024-01-11 16:55:40 -0800

    Update signup.js

--- src/components/signup.js
+++ src/components/signup.js
@@ -12,7 +12,7 @@ const Signup = () => {
         <p>Join 1,100+ others in learning the latest indie tech trends and strategies.</p>
       </div>
       <div className="signup">
-        <iframe src="https://smalltechbusiness.substack.com/embed" width="480" height="80"
+        <iframe src="https://smalltechbusiness.substack.com/embed" width="480" height="102"
                 style={{border:'1px solid transparent', background:'white'}} frameBorder="0" scrolling="no"></iframe>
       </div>
     </>
